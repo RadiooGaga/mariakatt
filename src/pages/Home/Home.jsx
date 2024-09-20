82bb51b5d537fcf0f119085b5c1a3440be9f5119
RadiooGaga <mariahkatt@gmail.com>
2024-09-20 14:49:50 +0200

    public

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
 
   return (
     <section className='homeCover'>
-      <img className='coverImage' src="/public/assets/pics/orange puppet.jpg" alt="coverImage" />
+      <img className='coverImage' src="/assets/pics/orange puppet.jpg" alt="coverImage" />
       <Header />
     </section>
   )
