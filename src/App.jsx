f06328baa0224ba2392604a13c94ec1a30e3b2e5
RadiooGaga <mariahkatt@gmail.com>
2024-09-27 19:02:10 +0200

    imposible cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
-
 import { Home } from './pages/Home/Home'
 import { Bio } from './pages/Bio/Bio';
 import { Editorial } from './pages/Editorial/Editorial'
