14a95553a31ef0baa756a304bf7eafa0b5668c7b
mariakatt <mariahkatt@gmail.com>
2025-03-27 18:52:12 +0100

    revision css

--- src/pages/Fx/Fx.jsx
+++ src/pages/Fx/Fx.jsx
@@ -8,8 +8,9 @@ const galleryItems = [
 
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743072319/makeup/fx/halloween2020_cqdw6j.webp`, caption: "Posesión - prótesis de silicona y dentadura", className: 'posesion', color: "whitesmoke" },
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743068826/makeup/fx/indice_ta6d8b.webp`, caption: "Reproducción dedo índice en silicona y maquillaje hiperrealista",className: 'dedo', color: "whitesmoke" },
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743081186/makeup/fx/ojo_zlmjv9.webp`, caption: "", className: 'ojo', color: "whitesmoke" },
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743071015/makeup/fx/cicatriz-quemadura_rk1zo0.webp`, caption: "Cicatriz queloide por quemadura - prótesis planas ", className: 'vertical',  color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743081186/makeup/fx/ojo_zlmjv9.webp`, caption: "", className: 'ojo', color: "whitesmoke" },
+
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743078891/makeup/fx/Victor-Castllo-euv_k5r5es.webp`, caption: '"El último vaso"',  color: "whitesmoke" },
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743079011/makeup/fx/accidente_oarecw.webp`, caption: '"El último vaso"',className: 'accidente2',   color: "whitesmoke" },
 
