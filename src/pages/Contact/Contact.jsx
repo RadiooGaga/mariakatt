1dd8f8127f4dfd688c6d007bf5337d7830054a58
mariakatt <mariahkatt@gmail.com>
2025-05-29 18:15:37 +0200

    contacto momentaneo

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
+import { useNavigate } from 'react-router-dom'
 
 export const Contact = () => {
+
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+      navigate('/')
+    }
+
   return (
-    <div>Contact</div>
-  )
+    <section className='contactSection'>
+      <h2 className='warning'>ESTA WEB ESTA EN CONSTRUCCION, PERO PUEDES ENVIARME TU MENSAJE A ESTE MAIL </h2>
+      <h3 className='mail'>mariahkatt@gmail.com</h3>
+      <span>🫠</span>
+      <h2 className='volver' onClick={handleGoBack}>VOLVER</h2>
+    </section>
+  );
 }
