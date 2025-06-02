4d825f29253120d474d019c50b64b0f4326cb6d4
mariakatt <mariahkatt@gmail.com>
2025-06-02 17:07:59 +0200

    nonstop gallery items

--- src/utils/carrouselItems.js
+++ src/utils/carrouselItems.js
@@ -2,14 +2,23 @@ import { micloudinary } from '../utils/config'
 
 export const carrouselItems = [
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444204/makeup/celebrities/Javier-Botet-y-Patricia-Vazquez_iielu8.webp`},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748874526/makeup/making%20of/RDR_75_tpudl8.jpg`},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746443458/makeup/celebrities/Julia%CC%81n-Lopez_kl0ipr.webp`},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748876640/makeup/making%20of/bill_mnggpv.png`},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746443689/makeup/celebrities/Aria-Bedmar_atznxp.webp`, class: 'adjusted1'},
-  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746443800/makeup/celebrities/Angy-Fernandez_rdrjhq.webp`, class: 'adjusted1'},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748876172/makeup/making%20of/297284_10150324058632623_2115894562_n_jaoud4.jpg`, class: 'adjusted1'},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748876638/makeup/making%20of/hans_hin7wy.png`},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748874607/makeup/making%20of/RDR_101_l97lrj.jpg`},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444011/makeup/celebrities/Goyo-Jimenez_njmemv.webp`},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444352/makeup/celebrities/Cristina-Brondo_vgwqli.webp`,class: 'adjusted1'},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444484/makeup/celebrities/Carlos-Manuel-Diaz_g1tawb.webp`, class: 'adjusted1'},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444724/makeup/celebrities/Chacha-Huang_oaszkj.webp`, class: 'adjusted2'},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1746444974/makeup/celebrities/Mad-Max-Payne_vvegx3.webp`, class: 'adjusted3'},
   {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748874145/makeup/celebrities/leo_20harlem_mfdz3x.jpg`, class: 'adjusted3'},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748875448/makeup/making%20of/374654_457983194286678_926651928_n_daiuld.jpg`, class: 'adjusted2'},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748875584/makeup/making%20of/corto_2022_jornada_2_26_kea004.jpg`, class: 'adjusted4'},
+  {img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1748876150/makeup/making%20of/1001898_10203488693362509_1723496236_n_rdlkok.jpg`, class: 'adjusted3'},
+  
+
   
 ];
