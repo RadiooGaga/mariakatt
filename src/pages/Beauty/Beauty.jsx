b9f36b3db15695639dc9785b29b2b07a1a22128b
mariakatt <mariahkatt@gmail.com>
2025-11-25 11:32:35 +0100

    wip

--- src/pages/Beauty/Beauty.jsx
+++ src/pages/Beauty/Beauty.jsx
@@ -8,15 +8,16 @@ import { Gallery } from '../../components/Gallery/Gallery';
 
 const galleryItems = [
 
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745573122/makeup/beauty/beauty1_bzems7.webp`, caption: "Maquillaje de noche/fiesta", className: 'beauty1', color: "whitesmoke" },
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1742821852/makeup/beauty/90s_agmwll.webp`, caption: "", className: 'beauty2', color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764062526/makeup/editorial/marina-ppp.ig_nwbpdo.jpg`, caption: "Marina - hair & makeup", className: 'beauty9', color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1742821852/makeup/beauty/90s_agmwll.webp`, caption: "", className: 'beauty2', color: "whitesmoke" }, 
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743066856/makeup/beauty/ss-model_ebvkw8.webp`, caption: "Book - hair & makeup",className: 'beauty3', color: "whitesmoke" },
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743066683/makeup/beauty/makeup_natural2_yuxndp.webp`, caption: "Maquillaje invitada", className: 'beauty4', color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745573122/makeup/beauty/beauty1_bzems7.webp`, caption: "Maquillaje de noche/fiesta", className: 'beauty1', color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764062534/makeup/editorial/mar-ppp-ig_oyrwfw.jpg`, caption: "Mar - hair & makeup", className: 'beauty10', color: "whitesmoke" },
+  /*{ img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743066683/makeup/beauty/makeup_natural2_yuxndp.webp`, caption: "Maquillaje invitada", className: 'beauty4', color: "whitesmoke" },*/
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1742833384/makeup/social/Screenshot_20230509_204509_g5gtp1.jpg`, caption: "Sara García - hair & makeup", className: 'beauty5', color: "whitesmoke" },
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743098838/makeup/beauty/Laia_Alemany_3_ucpgp5.webp`, caption: "Laia Alemany - hair & makeup", className: 'beauty6', color: "whitesmoke"},
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743074095/makeup/beauty/invitada1makeup_slyexz.webp`, caption: "Invitada -hair & makeup", className: 'beauty7', color: "whitesmoke" },
+  /*{ img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1743074095/makeup/beauty/invitada1makeup_slyexz.webp`, caption: "Invitada - hair & makeup", className: 'beauty7', color: "whitesmoke" },*/
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1742832832/makeup/social/makeup_natural_3_copia_yalpe9.webp`, caption: "Invitada - hair & makeup", className: 'beauty8', color: "whitesmoke" },
-
 ];
 
 
--- src/pages/Editorial/Editorial.jsx
+++ src/pages/Editorial/Editorial.jsx
@@ -6,16 +6,15 @@ import { Gallery } from '../../components/Gallery/Gallery';
 
 
 const galleryItems = [
-    
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490171/makeup/editorial/editorial1_t3mgmf.webp`, caption: "Saako design - hair&makeup", className: "editorial1", color: "whitesmoke" },
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490335/makeup/editorial/editorial2_po1ukv.webp`, caption: "Florenz Collection - hair&makeup", className: "editorial2", color: "whitesmoke" },
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490502/makeup/editorial/editorial3_n4bho5.webp`, caption: "Brianda Fitz-James - hair&makeup para Cucareliquia",className: "editorial3", color: "whitesmoke"},
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745489985/makeup/editorial/editorial4_s9asnu.webp`, caption: "El País Semanal - hair&makeup", className: "editorial4", color: "whitesmoke"},
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490627/makeup/editorial/editorial5_l4wasz.webp`, caption: "Florenz Collection - hair&makeup", className: "editorial5", color: "whitesmoke"  },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764062521/makeup/editorial/marina-pp-ig_v7f7vy.jpg`, caption: "Market Editorial - hair&makeup", className: "editorial0", color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764065600/makeup/editorial/EPS_STREETSTYLE_160855ok-001_compressed_qz9bzh.webp`, caption: "El País Semanal - hair&makeup", className: "editorial8", color: "whitesmoke" },
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764062974/makeup/editorial/Cucareliquia_Scarves_by_Brianda_Fitz-James_Stuart_7_copia_byedm7.jpg`, caption: "Brianda Fitz-James - hair&makeup para Cucareliquia",className: "editorial3", color: "whitesmoke"},
+  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1764062905/makeup/editorial/EPS_STREETSTYLE_161203ok-005_compressed_ueopjx.webp`, caption: "El País Semanal - hair&makeup", className: "editorial4", color: "whitesmoke"},
   { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490839/makeup/editorial/editorial6_kqufpo.webp`, caption: "El País Semanal - hair&makeup", className: "editorial6", color: "whitesmoke"},
-  { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745489580/makeup/editorial/editorial7_qorapv.webp`, caption: "Florenz Collection - makeup", className: "editorial7", color: "whitesmoke"}
-
-
+    { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490627/makeup/editorial/editorial5_l4wasz.webp`, caption: "Florenz Collection - hair&makeup", className: "editorial5", color: "whitesmoke"  },
+  /*{ img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745489580/makeup/editorial/editorial7_qorapv.webp`, caption: "Florenz Collection - makeup", className: "editorial7", color: "whitesmoke"},*/
+ { img: `https://res.cloudinary.com/${micloudinary}/image/upload/v1745490171/makeup/editorial/editorial1_t3mgmf.webp`, caption: "Saako design - hair&makeup", className: "editorial1", color: "whitesmoke" },
 ];
 
 export const Editorial = () => {
